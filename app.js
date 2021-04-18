95a9fd0ea01671df165cc497c73b96cf19053ec7
Victor Shevchuk <v.shevchuk@viseven.com>
2021-04-18 08:59:10 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// const ids = new Set(['hi', 'from', 'set!']);
+// ids.add(2);
+// ids.delete(2);
+
+// for (const entry of ids.values()) {
+//   console.log(entry);
+// }
+
+// const person1 = { name: 'Max' };
+// const person2 = { name: 'Manuel' };
+
+// const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
+
+// personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
+
+// // console.log(personData);
+// // console.log(personData.get(person1));
+
+// for (const [key, value] of personData.entries()) {
+//   // console.log(key);
+//   // console.log(value);
+// }
+
+// for (const key of personData.keys()) {
+//   console.log(key);
+// }
+
+let person = { name: 'Max' };
+const persons = new WeakSet();
+persons.add(person);
+// person = null;
+console.log(persons);
+
+const personData = new WeakMap();
+personData.set(person, 'Extra info!');
+person = null;
+console.log(personData);
